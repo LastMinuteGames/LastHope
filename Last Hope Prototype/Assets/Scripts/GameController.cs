6f203e4c3b957dcb55af8bcb21d6fdd4d2156da2
Manuel <troyanlp@gmail.com>
2017-03-12 17:52:03 +0100

    Prefabs of "Main Menu" and "In Game Menu". Menu In Game appears and disappears with 'Esc'.

---
+++ Last Hope Prototype/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Canvas menuInGame;
+    public bool isMenu = false;
+
+	// Use this for initialization
+	void Start () {
+        menuInGame = menuInGame.GetComponent<Canvas>();
+        menuInGame.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isMenu)
+        {
+            menuInGame.gameObject.SetActive(true);
+            Time.timeScale = 0.0F;
+            isMenu = true;
+        }else if (Input.GetKeyDown(KeyCode.Escape) && isMenu)
+        {
+            menuInGame.gameObject.SetActive(false);
+            Time.timeScale = 1.0F;
+            isMenu = false;
+        }
+    }
+}
