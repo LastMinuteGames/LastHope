9e5001ceafe4cfd29e6b0b5d4349750a8358c232
SergiLedesma <sergi.ledesmasanchez@gmail.com>
2017-04-10 20:47:55 +0200

    Pickup prefab changed into 4 prefabs, one for each type and effect.

--- Last Hope Prototype/Assets/Scripts/PickUpController.cs
+++ Last Hope Prototype/Assets/Scripts/PickUpController.cs
@@ -18,44 +18,6 @@ public class PickUpController : MonoBehaviour {
     public PickUpEffect effect;
     public PickUpType type;
     public int value;
-    public Material currentHpMat;
-    public Material maxHpMat;
-    public Material currentEnergyMat;
-    public Material maxEnergyMat;
-
-    private Renderer rend;
-
-    void Start ()
-    {
-        rend = GetComponent<Renderer>();
-        rend.enabled = true;
-        switch (effect)
-        {
-            case PickUpEffect.CURRENT:
-                switch (type)
-                {
-                    case (PickUpType.HP):
-                        rend.sharedMaterial = currentHpMat;
-                        break;
-                    case (PickUpType.ENERGY):
-                        rend.sharedMaterial = currentEnergyMat;
-                        break;
-                }
-                break;
-            case PickUpEffect.MAX:
-
-                switch (type)
-                {
-                    case (PickUpType.HP):
-                        rend.sharedMaterial = maxHpMat;
-                        break;
-                    case (PickUpType.ENERGY):
-                        rend.sharedMaterial = maxEnergyMat;
-                        break;
-                }
-                break;
-        }
-    }
     
     void Update () {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
