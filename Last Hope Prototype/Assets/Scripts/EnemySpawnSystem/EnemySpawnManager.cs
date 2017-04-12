3ceea092b5de791986cd112539d89687f53b58f9
SergiLedesma <sergi.ledesmasanchez@gmail.com>
2017-04-12 19:32:29 +0200

    Enemy spawn manager, point and trigger setup with functionality working. Customizable delay before spawn, number of enemies, type, etc.

---
+++ Last Hope Prototype/Assets/Scripts/EnemySpawnSystem/EnemySpawnManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyType
+{
+    ET_TRASH,
+    ET_MELEE,
+    ET_RANGED
+}
+
+public class EnemySpawnManager : MonoBehaviour {
+    public GameObject trashEnemy;
+    public GameObject meleeEnemy;
+    public GameObject rangedEnemy;
+
+    private List<GameObject> enemies;
+
+    public GameObject SpawnEnemy(Transform transform, EnemyType type)
+    {
+        GameObject originalEnemy;
+        switch (type)
+        {
+            case EnemyType.ET_TRASH:
+                originalEnemy = trashEnemy;
+                break;
+            case EnemyType.ET_MELEE:
+                originalEnemy = meleeEnemy;
+                break;
+            case EnemyType.ET_RANGED:
+                originalEnemy = rangedEnemy;
+                break;
+            default:
+                originalEnemy = trashEnemy;
+                break;
+        }
+        GameObject spawnedEnemy = Instantiate(originalEnemy, transform.position, transform.rotation);
+        //enemies.Add(spawnedEnemy);
+        return spawnedEnemy;
+    }
+
+    public void DestroyEnemy(GameObject enemy)
+    {
+        enemies.Remove(enemy);
+        Destroy(enemy);
+    }
+}
---
+++ Last Hope Prototype/Assets/Scripts/EnemySpawnSystem/EnemySpawnTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnTrigger : MonoBehaviour {
+    public List<GameObject> spawnPoints;
+    public EnemySpawnManager manager;
+
+    private bool spawning = false;
+    
+	void Start () {
+        
+    }
+	
+	void Update () {
+		if (spawning)
+        {
+            for (int i = 0; i < spawnPoints.Count; ++i)
+            {
+                if (spawnPoints[i].GetComponent<EnemySpawnPoint>().delay <= 0)
+                {
+                    manager.SpawnEnemy(spawnPoints[i].transform, spawnPoints[i].GetComponent<EnemySpawnPoint>().type);
+                    Destroy(spawnPoints[i]);
+                    spawnPoints.RemoveAt(i);
+                } else
+                {
+                    spawnPoints[i].GetComponent<EnemySpawnPoint>().delay -= Time.deltaTime;
+                }
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        spawning = true;
+    }
+}
