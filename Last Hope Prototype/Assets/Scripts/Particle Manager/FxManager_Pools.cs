e1dfc5dd7f36fc9da4ec7f6b03fd3eb7d6226f7c
Manuel <troyanlp@gmail.com>
2017-05-17 16:54:12 +0200

    Added Fx system scripts. Player prefab modified. Pool not working properly. Particle called when space is pressed.

---
+++ Last Hope Prototype/Assets/Scripts/Particle Manager/FxManager_Pools.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FxManager_Pools : MonoBehaviour
+{
+	public static FxManager_Pools Instance;
+
+	public Transform FxDefaultParent;
+
+	private Dictionary<GameObject, List<GameObject>> mPoolsDictionary;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	void Start()
+	{
+		mPoolsDictionary = new Dictionary<GameObject, List<GameObject>>();
+	}
+
+	public GameObject GetFx(GameObject aPrefab, Vector3 aPosition)
+	{
+		return GetFxFromPool(aPrefab, aPosition);
+	}
+
+	public GameObject GetFx(GameObject aPrefab, Transform aParent)
+	{
+		return GetFxFromPool(aPrefab, aParent.position, aParent);
+	}
+
+	public GameObject GetFxFromPool(GameObject aPrefabType, Vector3 aPositionToSpawn, Transform aParent = null)
+	{
+		if (aPrefabType == null)
+		{
+			return null;
+		}
+
+		GameObject lFx = null;
+		List<GameObject> lFxList;
+
+		if (mPoolsDictionary.TryGetValue(aPrefabType, out lFxList))
+		{
+			if (lFxList != null)
+			{
+				for (int i=0; i<lFxList.Count; ++i)
+				{
+					if (lFxList[i] != null && !lFxList[i].gameObject.activeInHierarchy)
+					{
+						//nos vale esta instancia, la reusamos!
+						lFx = lFxList[i];
+
+						break;
+					}
+				}
+			}
+		}
+
+		//no se encontró instancia válida, hay que instanciar
+		if (lFx == null)
+		{
+			lFx = Instantiate(aPrefabType) as GameObject;
+
+			if (mPoolsDictionary.TryGetValue(aPrefabType, out lFxList))
+			{
+				mPoolsDictionary[aPrefabType].Add(lFx);
+			}
+			else
+			{
+				lFxList = new List<GameObject>();
+				lFxList.Add(lFx);
+				mPoolsDictionary.Add(aPrefabType, lFxList);
+			}
+		}
+
+		lFx.SetActive(true);
+
+		lFx.transform.position = aPositionToSpawn;
+
+		if (aParent != null)
+		{
+			lFx.transform.parent = aParent;
+		}
+		else
+		{
+			lFx.transform.parent = FxDefaultParent;
+		}
+
+		return lFx;
+	}
+}
---
+++ Last Hope Prototype/Assets/Scripts/Particle Manager/FxReceiver_Basic.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FxReceiver_Basic : MonoBehaviour
+{
+	public Transform HitEmitter;
+
+	// ... Aquí iría todo el código de la lógica de la entidad
+}
