10b87d0a25eee0028ae218c7354206836bcca7ef
SergiLedesma <sergi.ledesmasanchez@gmail.com>
2017-05-24 20:33:51 +0200

    Player attack behaviour replicated for all attack states of the combo system sub-machine.

--- Last Hope Prototype/Assets/Scripts/Player/AnimatorBehaviours/FakePlayerHeavyAttack3.cs
+++ Last Hope Prototype/Assets/Scripts/Player/AnimatorBehaviours/FakePlayerHeavyAttack3.cs
@@ -2,30 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FakePlayerHeavyAttack3 : StateMachineBehaviour {
+public class FakePlayerHeavyAttack3 : StateMachineBehaviour
+{
+    PlayerController playerController;
 
-	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (playerController == null)
+        {
+            playerController = animator.transform.gameObject.GetComponent<PlayerController>();
+        }
+        playerController.CloseInputWindow();
+    }
 
-	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (playerController.GetInputWindowState())
+        {
+            if (InputManager.LightAttack())
+            {
+                animator.SetTrigger("lightAttack");
+            }
+            else if (InputManager.HeavyAttack())
+            {
+                animator.SetTrigger("heavyAttack");
+            }
+        }
+    }
 
-	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerController.CloseInputWindow();
+    }
 
-	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
 
-	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
 }
