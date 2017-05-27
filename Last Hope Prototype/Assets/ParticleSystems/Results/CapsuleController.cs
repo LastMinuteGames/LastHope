078157118ffd0ebcaa8e9ec7791855ad810d82fd
SergiLedesma <sergi.ledesmasanchez@gmail.com>
2017-05-27 14:45:30 +0200

    Particle systems imported from a test project. Explosion and enemy arrival changed to match new enemy models sizes.

---
+++ Last Hope Prototype/Assets/ParticleSystems/Results/CapsuleController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleController : MonoBehaviour
+{
+    public ParticleSystem landingParticles;
+    public float heightOffset;
+    public float fallDuration;
+    public GameObject enemy;
+    public GameObject decal;
+
+    private float destinationY;
+    private Vector3 startOffset;
+    private float interpolationTime = 0;
+    private float currentHeight;
+    private float capsuleHeight;
+
+    void Start()
+    {
+        startOffset = new Vector3(0, heightOffset, 0);
+        destinationY = gameObject.transform.position.y;
+        gameObject.transform.position += startOffset;
+        capsuleHeight = GetComponentInParent<CapsuleCollider>().height;
+    }
+
+    void Update()
+    {
+        currentHeight = Mathf.Lerp(startOffset.y, destinationY, interpolationTime);
+        if (interpolationTime <= 1)
+        {
+            interpolationTime += Time.deltaTime / fallDuration;
+        }
+        Vector3 currentPos = new Vector3(gameObject.transform.position.x, currentHeight, gameObject.transform.position.z);
+        gameObject.transform.position = currentPos;
+        if (currentHeight <= destinationY)
+        {
+            SpawnParticles();
+            SpawnDecal();
+            Destroy(gameObject);
+        }
+    }
+
+    void SpawnParticles()
+    {
+        Vector3 landingPos = transform.position - new Vector3(0, destinationY, 0);
+        Instantiate(landingParticles, transform.position, transform.rotation);
+        Instantiate(enemy, landingPos, transform.rotation);
+    }
+
+    void SpawnDecal()
+    {
+        float verticalRotation = Random.Range(0, 360);
+        Quaternion hitRotation = Quaternion.Euler(90, verticalRotation, 0);
+        float verticalPosition = capsuleHeight / 2 * transform.localScale.y - 0.001f;
+        GameObject a = Instantiate(decal, transform.position - new Vector3(0, verticalPosition, 0), hitRotation);
+    }
+}
---
+++ Last Hope Prototype/Assets/VerticalAxisRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalAxisRotation : MonoBehaviour {
+    private bool rotating = false;
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            rotating = !rotating;
+        }
+        if (rotating)
+        {
+            transform.Rotate(0, 0.5f, 0);
+        }
+	}
+}
