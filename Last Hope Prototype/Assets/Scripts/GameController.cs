4dfe8fb2828da9720dfb751187a007f71ab1d38e
Manuel <troyanlp@gmail.com>
2017-05-29 03:32:34 +0200

    First try implementing a camera shake.

--- Last Hope Prototype/Assets/Scripts/GameController.cs
+++ Last Hope Prototype/Assets/Scripts/GameController.cs
@@ -6,11 +6,15 @@ public class GameController : MonoBehaviour {
 
     public Canvas menuInGame;
     public bool isMenu = false;
+    public Camera camera;
     
 	void Start () {
         Time.timeScale = 1;
-        menuInGame = menuInGame.GetComponent<Canvas>();
-        menuInGame.gameObject.SetActive(false);
+        if(menuInGame != null)
+        {
+            menuInGame = menuInGame.GetComponent<Canvas>();
+            menuInGame.gameObject.SetActive(false);
+        }
         isMenu = false;
     }
 	
@@ -22,6 +26,13 @@ public class GameController : MonoBehaviour {
         else if (InputManager.Pause() && isMenu)
         {
             closeMenu();
+        }
+
+        if (InputManager.Interact())
+        {
+            Debug.Log("Hola");
+            StartCoroutine(Shake()); 
+            Debug.Log("Adios");
         }
     }
 
@@ -38,4 +49,36 @@ public class GameController : MonoBehaviour {
         Time.timeScale = 1.0F;
         isMenu = false;
     }
+
+    IEnumerator Shake(float duration = 0.01f, float magnitude = 1f)
+    {
+
+        float elapsed = 0.0f;
+
+        //Vector3 originalCamPos = Camera.main.transform.position;
+        Vector3 originalCamPos = camera.transform.position;
+        Debug.Log("Dentro");
+
+        while (elapsed < duration)
+        {
+
+            elapsed += Time.deltaTime;
+
+            float percentComplete = elapsed / duration;
+            float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
+
+            // map value to [-1, 1]
+            float x = Random.value * 2.0f - 1.0f;
+            float y = Random.value * 2.0f - 1.0f;
+            x *= magnitude * damper;
+            y *= magnitude * damper;
+
+            Camera.main.transform.position = new Vector3(x, y, originalCamPos.z);
+
+            yield return null;
+        }
+
+        Camera.main.transform.position = originalCamPos;
+    }
+
 }
