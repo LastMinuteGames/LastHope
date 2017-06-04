8c6a2810fdf0d629728af19319a5e4538ca05292
Manuel <troyanlp@gmail.com>
2017-06-04 17:22:47 +0200

    Settings menu now has visual feedback of which option has the focus.

--- Last Hope Prototype/Assets/Scripts/SettingsMenu.cs
+++ Last Hope Prototype/Assets/Scripts/SettingsMenu.cs
@@ -5,26 +5,49 @@ using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour {
 
+    public Text qualityText;
+    public Text volumeText;
     public Slider qualitySlider;
     public Slider volumeSlider;
     public Text qualityValue;
     private int focus = 0;
+    public Color unselectedTextColor;
+    public Color selectedTextColor;
 
     // Use this for initialization
     void Start () {
         qualityValue.text = QualitySettings.names[QualitySettings.GetQualityLevel()];
         qualitySlider.value = QualitySettings.GetQualityLevel();
+
+        unselectedTextColor = new Color(0F, 0F, 0F, 1);
+        selectedTextColor = new Color(1F, 1F, 1F, 1);
+
+        qualityText.color = selectedTextColor;
+        qualityValue.color = selectedTextColor;
+        volumeText.color = unselectedTextColor;
     }
 
     void Update()
     {
         if (InputManager.LeftJoystickUp() || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            if (focus == 1) focus--;
+            if (focus == 1)
+            {
+                focus--;
+                qualityText.color = selectedTextColor;
+                qualityValue.color = selectedTextColor;
+                volumeText.color = unselectedTextColor;
+            }
         }
         if (InputManager.LeftJoystickDown() || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if (focus == 0) focus++;
+            if (focus == 0)
+            {
+                focus++;
+                qualityText.color = unselectedTextColor;
+                qualityValue.color = unselectedTextColor;
+                volumeText.color = selectedTextColor;
+            }
         }
 
         if(focus == 0)
