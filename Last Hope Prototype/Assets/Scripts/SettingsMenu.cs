747497846cfea79ddcf6c8444179e07f4d53f6f4
Manuel <troyanlp@gmail.com>
2017-06-04 15:58:53 +0200

    Settings menu done. Quality slider shows the name of the quality setting selected and changes the quality level for the whole game. Volume slider changes the volume for the whole game.

---
+++ Last Hope Prototype/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public Slider qualitySlider;
+    public Slider volumeSlider;
+    public Text qualityValue;
+
+    // Use this for initialization
+    void Start () {
+        qualityValue.text = QualitySettings.names[QualitySettings.GetQualityLevel()];
+        qualitySlider.value = QualitySettings.GetQualityLevel();
+    }
+
+    public void Quality_Changed(float newValue)
+    {
+        qualityValue.text = QualitySettings.names[(int)newValue];
+        QualitySettings.SetQualityLevel((int)newValue);
+    }
+
+    public void Volume_Changed(float newValue)
+    {
+        Debug.Log(newValue);
+        //sound.volume = newValue;
+        AudioListener.volume = newValue;
+        
+    }
+
+}
