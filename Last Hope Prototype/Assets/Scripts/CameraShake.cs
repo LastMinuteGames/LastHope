cfba99d626ea22b774520a577b4e7797a6d91463
Manuel <troyanlp@gmail.com>
2017-06-06 15:22:19 +0200

    Camera Shake refactor done. Now it doesn't stacks camera shake calls. Now Camera Shake has the player transform. Camera shakes are only called when the caller is at a minimum distance (can be changed in inspector) or closer.

--- Last Hope Prototype/Assets/Scripts/CameraShake.cs
+++ Last Hope Prototype/Assets/Scripts/CameraShake.cs
@@ -7,19 +7,22 @@ public class CameraShake : MonoBehaviour {
 
     new Camera camera;
     public bool done = false;
-
+    private bool shaking = false;
+    public float distanceShake = 40f;
+    public Transform player;
     private Vector3 offsetToPivot;
 
 
     void Awake()
     {
         offsetToPivot = transform.localPosition;
+        
     }
 
     // Use this for initialization
     void Start () {
         camera = this.GetComponent<Camera>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -29,41 +32,66 @@ public class CameraShake : MonoBehaviour {
         }*/
     }
 
-    public IEnumerator Shake(float duration = 0.2f, float magnitude = 0.5f, float xMultiplier = 1.0f, float yMultiplier = 1.0f)
+    public IEnumerator Shake(float duration = 0.2f, float magnitude = 0.5f, float xMultiplier = 1.0f, float yMultiplier = 1.0f, Transform positionCalled = null)
     {
-        done = false;
-        if (this.GetComponent<CameraController>())
+        if (!shaking && CanShake(positionCalled))
         {
-            Debug.Log("A false!");
-            this.GetComponent<CameraController>().enabled = false;
-        }
-        float elapsed = 0.0f;
+            done = false;
+            shaking = true;
+            if (this.GetComponent<CameraController>())
+            {
+                this.GetComponent<CameraController>().enabled = false;
+            }
+            float elapsed = 0.0f;
 
-        while (elapsed < duration)
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+
+                float percentComplete = elapsed / duration;
+                float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
+
+                // map value to [-1, 1]
+                float x = Random.value * 2.0f - 1.0f;
+                float y = Random.value * 2.0f - 1.0f;
+                x *= magnitude * damper * xMultiplier;
+                y *= magnitude * damper * yMultiplier;
+
+                transform.localPosition = new Vector3(offsetToPivot.x + x, offsetToPivot.y + y, offsetToPivot.z);
+
+                yield return null;
+            }
+
+            transform.localPosition = offsetToPivot;
+            if (this.GetComponent<CameraController>())
+            {
+                this.GetComponent<CameraController>().enabled = true;
+            }
+            done = true;
+            shaking = false;
+
+        }
+        
+    }
+
+    public bool CanShake(Transform positionCalled)
+    {
+        if (!positionCalled)
         {
-            elapsed += Time.deltaTime;
-
-            float percentComplete = elapsed / duration;
-            float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
-
-            // map value to [-1, 1]
-            float x = Random.value * 2.0f - 1.0f;
-            float y = Random.value * 2.0f - 1.0f;
-            x *= magnitude * damper * xMultiplier;
-            y *= magnitude * damper * yMultiplier;
-
-            transform.localPosition = new Vector3(offsetToPivot.x + x, offsetToPivot.y + y, offsetToPivot.z);
-
-            yield return null;
+            return true;
         }
-
-        transform.localPosition = offsetToPivot;
-        if (this.GetComponent<CameraController>())
+        else
         {
-            Debug.Log("A true!");
-            this.GetComponent<CameraController>().enabled = true;
+            float distance = Vector3.Distance(positionCalled.position, player.position);
+            if (distance <= distanceShake)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
-        done = true;
     }
 
 }
