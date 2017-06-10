e77ab297bd4de129fcd7c46f3c1f12962eab712c
Manuel <troyanlp@gmail.com>
2017-06-10 12:34:03 +0200

    Music on CityMap plays on loop. Created Panels on HUD canvas to make the dialogue box. Added icon of button A. Created DialogueSystem GameObject on GameManagers. Created script DialogueSysem.cs with instance and method to add dialogue lines.

---
+++ Last Hope Prototype/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour {
+    public static DialogueSystem Instance { get; set; }
+    public List<string> dialogueLines = new List<string>();
+    // Use this for initialization
+    void Awake () {
+		if(Instance != null && Instance != this)
+        {
+            Destroy(Instance);
+        }
+        else
+        {
+            Instance = this;
+        }
+	}
+	
+	public void AddNewDialogue(string[] lines)
+    {
+        dialogueLines = new List<string>(lines.Length);
+        dialogueLines.AddRange(lines);
+    }
+}
