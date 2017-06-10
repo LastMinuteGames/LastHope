6ad97db4952ebfcebb249b8e06744597b40efa54
Manuel <troyanlp@gmail.com>
2017-06-10 15:40:32 +0200

    Now you can add from where the dialogue comes (i.e: information, generator, colonel...) and it will appear over the dialogue box. When '1' is pressed, a 2-part dialogue shows for testing purposes. Dialogues work properly showing the text, the info about where does the dialogue come and the button to press. Dialogue flow works correctly appearing, calling the next one pressing 'A' and disappearing when there are no more dialogues left. Still needs block the input from the player to avoid calling a light attack when calling the next dialogue.

--- Last Hope Prototype/Assets/Scripts/DialogueSystem.cs
+++ Last Hope Prototype/Assets/Scripts/DialogueSystem.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueSystem : MonoBehaviour {
     public static DialogueSystem Instance { get; set; }
     public List<string> dialogueLines = new List<string>();
+    public List<string> fromLines = new List<string>();
+    public GameObject dialogueBox;
+    public Text dialogueText;
+    public Text dialogueFrom;
+    private bool show;
+    private bool next;
+
     // Use this for initialization
     void Awake () {
 		if(Instance != null && Instance != this)
@@ -15,11 +23,54 @@ public class DialogueSystem : MonoBehaviour {
         {
             Instance = this;
         }
+        dialogueBox.SetActive(false);
 	}
+
+    void Update()
+    {
+        if (show && next)
+        {
+            next = false;
+            if (dialogueLines.Count > 0)
+            {
+                dialogueBox.SetActive(true);
+                dialogueText.text = dialogueLines[0];
+                dialogueFrom.text = fromLines[0];
+            }
+            else
+            {
+                dialogueBox.SetActive(false);
+                show = false;
+                next = false;
+            }
+
+        }
+
+        if (InputManager.LightAttack() || Input.GetKeyDown(KeyCode.Return) && show)
+        {
+            Debug.Log("ENTER!");
+            NextDialogue();
+        }
+    }
 	
-	public void AddNewDialogue(string[] lines)
+	public void AddNewDialogue(string[] lines, string[] from)
     {
         dialogueLines = new List<string>(lines.Length);
+        fromLines = new List<string>(from.Length);
         dialogueLines.AddRange(lines);
+        fromLines.AddRange(from);
+    }
+
+    public void ShowDialogue()
+    {
+        show = true;
+        next = true;
+    }
+
+    public void NextDialogue()
+    {
+        next = true;
+        dialogueLines.RemoveAt(0);
+        fromLines.RemoveAt(0);
     }
 }
