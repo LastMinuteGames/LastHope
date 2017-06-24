bf1b118d78e28218d46434ba28ab220712e3f37e
SergiLedesma <sergi.ledesmasanchez@gmail.com>
2017-06-24 21:12:57 +0200

    Visibility wall created (fog) and placed in the world.

---
+++ Last Hope Prototype/Assets/ParticleSystems/Scripts/ScrollingMaterial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingMaterial : MonoBehaviour {
+
+    public float xScrollSpeed = 0.5f;
+    public float yScrollSpeed = 0;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    void FixedUpdate()
+    {
+        float offsetX = Time.time * xScrollSpeed;
+        float offsetY = Time.time * yScrollSpeed;
+        rend.material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
