122ea34d5dc6724aef5f186beb57ee47c9cb1bb6
SergiLedesma <sergi.ledesmasanchez@gmail.com>
2017-07-10 21:27:08 +0200

    Unnecessary scripts deleted. Game times changed. Comented code deleted. GamePad rumble bug fixed. CityMapUpdate small enviroment changes.

--- Last Hope Prototype/Assets/Scripts/Player/AnimatorBehaviours/FakePlayerHeavyAttack1.cs
+++ Last Hope Prototype/Assets/Scripts/Player/AnimatorBehaviours/FakePlayerHeavyAttack1.cs
@@ -11,51 +11,6 @@ public class FakePlayerHeavyAttack1 : PlayerBaseAttackState
         availableAttacks.Add("redSpecialAttack");
         attackName = "H1";
     }
-    //PlayerController playerController;
-    //private float h, v = 0;
-
-    //// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    if (playerController == null)
-    //    {
-    //        playerController = animator.transform.gameObject.GetComponent<PlayerController>();
-    //    }
-    //    playerController.ChangeAttack("H1");
-    //    playerController.DisableSwordEmitter();
-    //    playerController.DisableShieldEmitter();
-    //    playerController.CloseInputWindow();
-    //    playerController.EndSwordAttack();
-    //    playerController.EndShieldAttack();
-
-    //    h = InputManager.LeftJoystick().x;
-    //    v = InputManager.LeftJoystick().z;
-    //    playerController.PendingMovement(h, v);
-    //}
-
-    //// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    if (InputManager.SpecialAttack())
-    //    {
-    //        switch (playerController.SpecialAttackToPerform())
-    //        {
-    //            case PlayerStanceType.STANCE_BLUE:
-    //                animator.SetTrigger("blueSpecialAttack");
-    //                break;
-    //            case PlayerStanceType.STANCE_RED:
-    //                animator.SetTrigger("redSpecialAttack");
-    //                break;
-    //        }
-    //    }
-    //}
-
-    //// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    playerController.CloseInputWindow();
-    //    playerController.EndSwordAttack();
-    //}
 
     //// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
     ////override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
--- Last Hope Prototype/Assets/Scripts/Player/AnimatorBehaviours/FakePlayerRespawn.cs
+++ Last Hope Prototype/Assets/Scripts/Player/AnimatorBehaviours/FakePlayerRespawn.cs
@@ -23,11 +23,6 @@ public class FakePlayerRespawn : StateMachineBehaviour {
         animator.SetBool("idle", true);
     }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    //
-    //}
-
     // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
     //
