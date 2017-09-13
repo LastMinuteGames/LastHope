d0e41355e6bbc64f819e6585274c11427fadbd79
Manuel <troyanlp@gmail.com>
2017-09-13 23:38:01 +0200

    New model elevator working. When the player steps outside the elevator a collider activates to prevent him from falling. Elevator has an animator (not working now, possibly animation issue).

---
+++ Last Hope Prototype/Assets/Scripts/Boss/OutOfElevator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfElevator : MonoBehaviour {
+
+    public MeshCollider fence;
+    public Animator elevator;
+    private bool closed = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!closed && other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            closed = true;
+            Debug.Log("Cierra!");
+            fence.enabled = true;
+            elevator.SetBool("close", true);
+        }
+    }
+}
