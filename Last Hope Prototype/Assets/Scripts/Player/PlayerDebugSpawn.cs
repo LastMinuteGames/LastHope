30bde05ae254198ab4471cac5a3e469f6e24575b
kamtxatka <pau.mamano@gmail.com>
2017-09-17 16:52:24 +0200

    Player debug Spawn system

---
+++ Last Hope Prototype/Assets/Scripts/Player/PlayerDebugSpawn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDebugSpawn : MonoBehaviour
+{
+    public PlayerDebugSpawnPoint spawnPoint;
+    public Transform[] spawnPoints;
+
+    private PlayerDebugSpawnPoint oldSpawnPoint;
+    private Transform playerT;
+
+    private void Start()
+    {
+        playerT = GameObject.FindGameObjectWithTag("Player").transform;
+        UpdateSpawn();
+    }
+
+    private void UpdateSpawn()
+    {
+        oldSpawnPoint = spawnPoint;
+        int i = (int)spawnPoint;
+        playerT.position = spawnPoints[i].position;
+    }
+
+    private void Update()
+    {
+        if (spawnPoint != oldSpawnPoint)
+        {
+            UpdateSpawn();
+        }
+    }
+}
+
+
+public enum PlayerDebugSpawnPoint
+{
+    Start,
+    Generator,
+    BeforeArtillery,
+    AfterArtillery,
+    BeforeBoss,
+    BossFight
+}
