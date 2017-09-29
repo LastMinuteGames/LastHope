a2de6f84a4cd910da60544cfdebf217ef24cb06c
DESKTOP-EVNNTJE\Lede7 <sergi.ledesmasanchez@gmail.com>
2017-09-29 20:05:49 +0200

    Particle systems scene showing fx (keys 1 to 8). Initial work on showing player and artilleries fxs.

---
+++ Last Hope Prototype/Assets/ParticleSystemSwitcher.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemSwitcher : MonoBehaviour
+{
+
+    private List<GameObject> particles = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            particles.Add(child.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Activate(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Activate(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Activate(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Activate(4);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Activate(5);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            Activate(6);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            Activate(7);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            Activate(8);
+        }
+    }
+
+    void Activate(int num)
+    {
+        foreach (GameObject particle in particles)
+        {
+            particle.GetComponent<ParticleSystem>().Stop();
+            particle.gameObject.SetActive(false);
+        }
+        particles[num - 1].gameObject.SetActive(true);
+        particles[num - 1].GetComponent<ParticleSystem>().Play();
+    }
+}
---
+++ Last Hope Prototype/Assets/RotationScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+    public float verticalRotationRate = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, verticalRotationRate, 0));
+	}
+}
